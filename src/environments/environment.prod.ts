c3c997646773003ca634aca1b058bee3a8fe9239
Redidloo <samuel.semoda@orbiszlin.cz>
2026-03-30 09:14:34 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://app.beezy.cz/api',
-  apiKey: 'PQ09A6jptiDI1cLq6IpOL2uOctevRQbLyl9vrGGQq1p2ZWzA6SETO6wnP4FcqkU0',
-  appUrl: 'https://beeinformed-beezy.web.app'
+  apiUrl: 'API_URL',
+  apiKey: 'API_KEY',
+  appUrl: 'APP_URL'
 };
